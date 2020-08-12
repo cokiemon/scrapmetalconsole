b482625d83cab50c9cebc78e278ce8080043aebb
Nathaniel Acut <nathaniel.acut@gmail.com>
2020-08-12 15:49:09 +0800

    Move console loggers to its own class

--- scrapmetalconsole/ConsoleLogger.cs
+++ scrapmetalconsole/ConsoleLogger.cs
@@ -1,60 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-
-//using HtmlAgilityPack;
-//using ScrapySharp.Extensions;
-//using ScrapySharp.Network;
-
-using PuppeteerSharp;
+using System.Text;
 
 namespace scrapmetalconsole
 {
-    class Program
+    public static class ConsoleLogger
     {
-        public static async Task Main(string[] args)
-        {
-            var options = new LaunchOptions
-            {
-                Headless = true
-            };
-
-            Console.WriteLine("Downloading chromium");
-            await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
-
-            Console.WriteLine("Navigating google");
-            using (var browser = await Puppeteer.LaunchAsync(options))
-            using (var page = await browser.NewPageAsync())
-            {
-                string aliPageAddress = "https://www.aliexpress.com/item/4000366605357.html";
-
-                try
-                {
-                    await page.GoToAsync(aliPageAddress);
-
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex);
-                    Environment.Exit(1);
-                }
-
-                Store store = await StoreFactory.CreateStore(page);
-                //logToConsole(store);
-                ConsoleLogger.Log(store);
-                Console.WriteLine();
-
-                ProductInformation productInformation = await ProductInformation.CreateProductInformation(page);
-                //logToConsole(productInformation);
-                ConsoleLogger.Log(productInformation);
-            }
-
-            Environment.Exit(0);
-        }
-
-        private static void logToConsole(Store store)
+        public static void Log(Store store)
         {
             // For debug purposes only.
             Debug.WriteLine($"Store Name: {store.Name}");
@@ -64,7 +17,7 @@ namespace scrapmetalconsole
             Console.WriteLine($"Store Link: {store.Link}");
         }
 
-        private static void logToConsole(ProductInformation productInformation)
+        public static void Log(ProductInformation productInformation)
         {
             // For debug purposes only.
             Debug.WriteLine($"Title: {productInformation.TitleText}");
